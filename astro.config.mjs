5016b7ad4effa30d91ffb1f34311e339d4f0b5f4
Omar Ashour <omarashour02@gmail.com>
2026-05-01 00:21:10 +0300

    some changes in link preview

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from "@astrojs/mdx";
 import { gruvboxMaterialDark } from "./src/styles/shiki-theme.ts";
 
 export default defineConfig({
-  site: "https://example.pages.dev",
+  site: "https://notes.omarashour02.workers.dev",
   integrations: [react(), mdx()],
   markdown: {
     shikiConfig: {
